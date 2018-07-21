00619d87d52a262853b01d3bfb009235396781d7
aratama <16192627+aratama@users.noreply.github.com>
2018-07-21 22:46:59 +0900

    refactor scripts

--- src/elm-automata.mjs
+++ src/elm-automata.mjs
@@ -8,87 +8,85 @@ import { renderType } from "./template/type"
 import { renderRouter } from "./template/router";
 import { renderStyle } from "./template/style";
 
-const writeFile = util.promisify(fs.writeFile)
-
-async function getApplicationName(){
-  const filesInRoot = await util.promisify(fs.readdir)(`./src`)
+async function getApplicationName() {
+  const filesInRoot = await fs.readdir(`./src`)
 
   const dirsWithNull = await Promise.all(filesInRoot.map(async file => {
-    const stat = await util.promisify(fs.stat)(path.resolve('./src/', file))
+    const stat = await fs.stat(path.resolve('./src/', file))
     return stat.isDirectory() ? file : null
   }))
 
   const dirs = dirsWithNull.filter(dir => dir !== null)
 
-  if(dirs.length !== 1){
+  if (dirs.length !== 1) {
     throw new Error("Too many folders in src directory. Cannot decide application name.")
   }
 
-  const application = dirs[0]  
+  const application = dirs[0]
 
   return application
 }
 
-async function generateRouter(){
+async function generateRouter() {
 
   const application = await getApplicationName()
   console.log(`Found application: ${application}`)
 
-  const ds = await util.promisify(glob)(`./src/${application}/Page/**/`)  
-  
+  const ds = await util.promisify(glob)(`./src/${application}/Page/**/`)
+
   const pages = ds.map(dir => {
-    if(
-      fs.existsSync(path.resolve(dir, "style.css")) && 
-      fs.existsSync(path.resolve(dir, "Type.elm")) && 
-      fs.existsSync(path.resolve(dir, "Update.elm")) && 
-      fs.existsSync(path.resolve(dir, "View.elm")) 
-    ){
+    if (
+      fs.existsSync(path.resolve(dir, "style.css")) &&
+      fs.existsSync(path.resolve(dir, "Type.elm")) &&
+      fs.existsSync(path.resolve(dir, "Update.elm")) &&
+      fs.existsSync(path.resolve(dir, "View.elm"))
+    ) {
       return path.relative(`./src/${application}/Page/`, dir).split(path.sep)
-    }else{
+    } else {
       return null
     }
   }).filter(dir => dir !== null)
 
   // generate Routing.elm
-  console.log(`Generating ./src/${application}/Routing.elm for ${ pages.map(p => p.join(".") ).join(", ") }...`)
+  console.log(`Generating ./src/${application}/Routing.elm for ${pages.map(p => p.join(".")).join(", ")}...`)
   const source = renderRouter(application, pages)
-  await util.promisify(fs.writeFile)(`./src/${application}/Routing.elm`, source)
+  await fs.writeFile(`./src/${application}/Routing.elm`, source)
 
   // generate routing.js
   console.log(`Generating ./src/${application}/routing.js...`)
   const indexSource = renderStyle(application, pages)
-  await writeFile(path.resolve("./src/", application, "routing.js"), indexSource)
+  await fs.writeFile(path.resolve("./src/", application, "routing.js"), indexSource)
 
   console.log("Done.")
 }
 
-async function generateNewPage(pageName){
-  debugger
+async function generateNewPage(pageName) {
+
   console.log(`Generating new page: ${pageName}`)
 
   const application = await getApplicationName()
 
-  if(fs.existsSync(path.resolve(`./src/`, application, `Page`, pageName))){
+  if (fs.existsSync(path.resolve(`./src/`, application, `Page`, pageName))) {
     console.error(`[Error] Directory '${pageName}' already exists.`)
-  }else{
+  } else {
     const dir = path.resolve("./src/", application, "Page", pageName)
     await fs.ensureDir(dir)
-    await writeFile(path.resolve(dir, "style.css"), "")
-    await writeFile(path.resolve(dir, "Type.elm"), renderType(application, pageName))
-    await writeFile(path.resolve(dir, "Update.elm"), renderUpdate(application, pageName))
-    await writeFile(path.resolve(dir, "View.elm"), renderView(application, pageName))
+    await fs.writeFile(path.resolve(dir, "style.css"), "")
+    await fs.writeFile(path.resolve(dir, "Type.elm"), renderType(application, pageName))
+    await fs.writeFile(path.resolve(dir, "Update.elm"), renderUpdate(application, pageName))
+    await fs.writeFile(path.resolve(dir, "View.elm"), renderView(application, pageName))
 
     await generateRouter()
   }
 }
 
-async function main(){
+async function main() {
   const command = process.argv[2]
-  if(process.argv.length === 2){
+  if (process.argv.length === 2) {
     await generateRouter()
-  }else if (process.argv.length === 4 && command === "new"){
+  } else if (process.argv.length === 4 && command === "new") {
     await generateNewPage(process.argv[3])
-  }else{
+  } else {
     console.error(`Unknown command: ${process.argv.slice(2).join(" ")}`)
   }
 }
