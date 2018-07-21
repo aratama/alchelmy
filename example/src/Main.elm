07a6e0dfcae1bff60b4fb4dd7930e457e31a7a58
aratama <16192627+aratama@users.noreply.github.com>
2018-07-21 21:09:35 +0900

    cleanup example

--- example/src/Main.elm
+++ example/src/Main.elm
@@ -5,31 +5,11 @@ import Navigation exposing (Location)
 import CoolSPA.Routing as Routing exposing (Model, Msg, Route(..), parseLocation, navigate)
 
 
-init : Location -> ( Model, Cmd Msg )
-init =
-    Routing.init { theme = "goat" }
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update =
-    Routing.update
-
-
-view : Model -> Html Msg
-view =
-    Routing.view
-
-
-subscriptions : Model -> Sub Msg
-subscriptions =
-    Routing.subscriptions
-
-
 main : Program Never Model Msg
 main =
     Navigation.program navigate
-        { init = init
-        , view = view
-        , update = update
-        , subscriptions = subscriptions
+        { init = Routing.init { theme = "goat" }
+        , view = Routing.view
+        , update = Routing.update
+        , subscriptions = Routing.subscriptions
         }
