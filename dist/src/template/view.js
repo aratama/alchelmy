9eec323e2ad9cb0ffc671b75eb9cb7b720c5aba0
aratama <16192627+aratama@users.noreply.github.com>
2018-07-24 15:28:21 +0900

    fix build

--- dist/src/template/view.js
+++ dist/src/template/view.js
@@ -7,7 +7,8 @@ exports.renderView = renderView;
 function renderView(application, pageName) {
   return `module ${application}.Page.${pageName}.View exposing (..)
 
-import Html exposing (Html, text)
+import Html exposing (Html, text, a)
+import Html.Attributes exposing (href)
 import Html.Events exposing (onClick, onWithOptions)
 import Json.Decode exposing (succeed)
 import ${application}.Page.${pageName}.Type exposing (Model, Msg(..))
@@ -16,8 +17,5 @@ import ${application}.Type as Root
 view : Root.Model -> Model -> Html Msg
 view state model = text "${pageName}"
 
-navigate : String -> List (Html Msg) -> Html Msg
-navigate url contents =
-    a [ href url, onWithOptions "click" { stopPropagation = True, preventDefault = True } (succeed (Navigate url)) ] contents
 `;
 }
