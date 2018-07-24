d6b88bb870f4ed1b8cb505bf473c5899d1f669e4
aratama <16192627+aratama@users.noreply.github.com>
2018-07-24 21:37:53 +0900

    ci

--- src/elm-automata.mjs
+++ src/elm-automata.mjs
@@ -221,7 +221,7 @@ Options:
   } else if (command === "update") {
     await generateRouter(argv);
   } else if (command === "new") {
-    const pageName = process.argv[3];
+    const pageName = argv._[1];
     if (validatePageName(pageName)) {
       console.error(
         `Invalid page name: ${pageName}. An page name must be an valid Elm module name.`
