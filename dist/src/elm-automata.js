3024867ebfc2aa068a5b224d5144cfe672ca92af
aratama <16192627+aratama@users.noreply.github.com>
2018-07-24 21:40:57 +0900

    ci

--- dist/src/elm-automata.js
+++ dist/src/elm-automata.js
@@ -207,7 +207,7 @@ Options:
   } else if (command === "update") {
     await generateRouter(argv);
   } else if (command === "new") {
-    const pageName = process.argv[3];
+    const pageName = argv._[1];
     if (validatePageName(pageName)) {
       console.error(`Invalid page name: ${pageName}. An page name must be an valid Elm module name.`);
       console.error(JSON.stringify(argv, null, 2));
