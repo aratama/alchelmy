e1d03f94da2abdda30f2ed4e903cafa838f64057
aratama <16192627+aratama@users.noreply.github.com>
2018-07-29 19:49:21 +0900

    minor fixes

--- example/src/Main.elm
+++ example/src/Main.elm
@@ -1,8 +1,7 @@
 module Main exposing (..)
 
-import ElmPortfolio.Alchemy as Alchemy exposing (Model, Msg)
+import ElmPortfolio.Alchemy as Alchemy exposing (Model, Msg, program)
 
 
 main : Program Never Model Msg
-main =
-    Alchemy.program
+main = program
