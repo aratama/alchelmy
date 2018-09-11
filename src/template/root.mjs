e770e741b737ce97fea4ef6b335c3d2fbfea9a63
aratama <16192627+aratama@users.noreply.github.com>
2018-09-11 23:55:50 +0900

    update

--- src/template/root.mjs
+++ src/template/root.mjs
@@ -1,79 +1,42 @@
 export function renderRoot(application) {
   return `module ${application}.Root exposing (..)
 
-import UrlParser as UrlParser exposing (s, Parser, (</>), map, parseHash)
-import Navigation exposing (Location, newUrl)
-import Maybe exposing (withDefault)
-import Html exposing (Html, a)
-import Html.Attributes exposing (href)
-import Html.Events exposing (onWithOptions)
-import Json.Decode exposing (Decoder, andThen, bool, fail, field, map4, succeed)
-
+import Browser exposing (Document)
+import Browser.Navigation exposing (Key)
+import Url exposing (Url)
+import Url.Parser exposing (Parser)
 
 -- Application global state type.
 
 
 type alias Model =
-    {}
+    { key : Key }
   
 
 type Msg
-    = Navigate String
+    = NoOp
 
 
 type alias Page a route model msg =
-    { route : Parser (route -> a) a
-    , init : Location -> route -> Model -> (model, Cmd msg )
-    , update : msg -> Model -> model -> (Model, model, Cmd msg )
+    { route : Parser ( route -> a ) a
+    , init : Url -> route -> Model -> Key -> ( model, Cmd msg )
+    , update : msg -> Model -> model -> ( Model, model, Cmd msg )
     , subscriptions : Model -> Sub msg
-    , view : Model -> model -> Html msg
+    , view : Model -> model -> Document msg
     }
 
-init : Location -> ( Model, Cmd Msg )
-init _ =
-    ( {}, Cmd.none )
+init : Url -> Key -> ( Model, Cmd Msg )
+init _ key =
+    ( { key = key }, Cmd.none )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        Navigate url ->
-            (model, newUrl url)
+update msg model = (model, Cmd.none)
 
 
 subscriptions : Sub Msg
 subscriptions =
     Sub.none
 
-
-parse : Parser (a -> a) a -> Location -> Maybe a
-parse = parseHash
-    
-
-{-| Helper function for path routing. You can remove this function if you use Hash routing (\`parseHash\`).
--}
-navigate : (String -> msg) -> String -> List (Html msg) -> Html msg
-navigate msg url contents =
-    let
-        decoder : Decoder msg
-        decoder =
-            andThen
-                identity
-                (map4
-                    (\\ctrl shift alt meta ->
-                        if shift || ctrl || alt || meta then
-                            -- do browser default behavior
-                            fail ""
-
-                        else
-                            succeed (msg url)
-                    )
-                    (field "ctrlKey" bool)
-                    (field "shiftKey" bool)
-                    (field "altKey" bool)
-                    (field "metaKey" bool)
-                )
-    in
-    a [ href url, onWithOptions "click" { stopPropagation = True, preventDefault = True } decoder ] contents
 `.trimLeft();
 }
