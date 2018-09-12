2e4be2d7e1544b406e753cc76d145b3496e310b6
aratama <16192627+aratama@users.noreply.github.com>
2018-09-12 22:53:19 +0900

    refactor

--- bin.js
+++ bin.js
@@ -1,3 +1,11 @@
-#!/usr/bin/env node
-
-require("./dist/src/elm-alchemy.js");
+const { spawn } = require('child_process');
+const p = spawn('node', ['--experimental-modules', 'src/elm-alchemy.mjs']);
+p.stdout.on('data', (data) => {
+  console.log(data.toString());
+});
+p.stderr.on('data', (data) => {
+  console.error(data.toString());
+});
+p.on('close', (code) => {
+  process.exit(code);
+});
