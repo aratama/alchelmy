98e597dc01afc9964674085254d67ba21719548f
aratama <16192627+aratama@users.noreply.github.com>
2018-09-12 23:15:45 +0900

    fix bin

--- bin.js
+++ bin.js
@@ -1,7 +1,7 @@
 #! /usr/bin/env node
 const path = require("path");
 const { spawn } = require('child_process');
-const p = spawn('node', ['--experimental-modules', path.resolve(__dirname, 'src/elm-alchemy.mjs')]);
+const p = spawn('node', ['--experimental-modules', path.resolve(__dirname, 'src/elm-alchemy.mjs')].concat(process.argv.slice(2)));
 p.stdout.on('data', (data) => {
   console.log(data.toString());
 });
