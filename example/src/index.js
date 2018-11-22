dd252f0f0705caca0be848e7093bc0b9704708d8
aratama <16192627+aratama@users.noreply.github.com>
2018-11-22 18:15:36 +0900

    use sass

--- example/src/index.js
+++ example/src/index.js
@@ -1,12 +1,3 @@
-import './ElmPortfolio/Page/Counter.css'
-import './ElmPortfolio/Page/Http.css'
-import './ElmPortfolio/Page/Minimum.css'
-import './ElmPortfolio/Page/NotFound.css'
-import './ElmPortfolio/Page/Preferences.css'
-import './ElmPortfolio/Page/Time.css'
-import './ElmPortfolio/Page/Top.css'
-import './ElmPortfolio/Page/URLParsing.css'
-
 import "./main.css";
 import { Elm } from "./Main.elm";
 import { register, unregister } from "./registerServiceWorker";
